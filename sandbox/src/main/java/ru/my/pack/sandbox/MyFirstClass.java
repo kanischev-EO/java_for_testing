572a4309bbd8e57802163c9edc3f6ae5345724b4
pentogon <rukret@yandex.ru>
2021-07-12 22:47:36 +0300

    Добавил форматирование кода и правильный нейминг класса с обновленным файлом конфигурации в Gradle

--- sandbox/src/main/java/ru/my/pack/sandbox/MyFirstClass.java
+++ sandbox/src/main/java/ru/my/pack/sandbox/MyFirstClass.java
@@ -1,4 +1,6 @@
-public class Myfirstclass{
+package ru.my.pack.sandbox;
+
+public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
