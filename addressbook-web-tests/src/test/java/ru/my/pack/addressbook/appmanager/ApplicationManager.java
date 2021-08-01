299bb3164be50d760b99e3964c4ef8b611df4f11
pentogon <rukret@yandex.ru>
2021-08-01 19:43:40 +0300

    Добавил возможность запускать тесты  на разных браузерах

--- addressbook-web-tests/src/test/java/ru/my/pack/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/my/pack/addressbook/appmanager/ApplicationManager.java
@@ -2,26 +2,41 @@ package ru.my.pack.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   protected WebDriver wd;
 
-  private ContactHelper contactHelper ;
+  private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper ;
+  private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    System.setProperty("webdriver.chrome.driver", "/Users/ruanaoq/Downloads/chromedriver");
-    wd = new ChromeDriver();
+    if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    } else if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    contactHelper =  new ContactHelper(wd);
+    contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
 
--- addressbook-web-tests/src/test/java/ru/my/pack/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/my/pack/addressbook/test/TestBase.java
@@ -1,12 +1,13 @@
 package ru.my.pack.addressbook.test;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.my.pack.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
